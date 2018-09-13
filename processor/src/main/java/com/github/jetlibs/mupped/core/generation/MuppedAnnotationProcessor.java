fdf91c0c2178201d9269d8fee1551d4481709ae1
Konstantin Aksenov <konstantin.aksenov@agoda.com>
2018-09-13 11:45:13 +0700

    Fix build

--- processor/src/main/java/com/github/jetlibs/mupped/core/generation/MuppedAnnotationProcessor.java
+++ processor/src/main/java/com/github/jetlibs/mupped/core/generation/MuppedAnnotationProcessor.java
@@ -1,23 +1,19 @@
 package com.github.jetlibs.mupped.core.generation;
 
 import com.github.jetlibs.mupped.annotations.MapTo;
-import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-
-//@SupportedSourceVersion(SourceVersion.RELEASE_6)
-@AutoService(Processor.class)
 public class MuppedAnnotationProcessor extends AbstractProcessor {
 
-
     private Messager messager;
-
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
@@ -27,10 +23,9 @@ public class MuppedAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-  //      throw new RuntimeException("Blyat'");
         for (Element element : roundEnv.getElementsAnnotatedWith(MapTo.class)) {
+            messager.printMessage(Diagnostic.Kind.NOTE, "Found class: " + element.getSimpleName().toString());
             if (element.getKind() != ElementKind.CLASS) {
-              //  messager.printMessage(Diagnostic.Kind.NOTE, element.getSimpleName().toString());
                 return true;
             }
         }
@@ -40,12 +35,12 @@ public class MuppedAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
-        Set<String> annotataions = new LinkedHashSet<>();
+        final Set<String> annotataions = new LinkedHashSet<>();
         annotataions.add(MapTo.class.getCanonicalName());
         return annotataions;
     }
 
-//    @Override public SourceVersion getSupportedSourceVersion() {
-//        return SourceVersion.latestSupported();
-//    }
+    @Override public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
 }
