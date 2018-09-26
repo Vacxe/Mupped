80c163e6b050dee01dd21b515a7975c7e30c0508
Konstantin Aksenov <konstantin.aksenov@agoda.com>
2018-09-26 18:49:24 +0700

    Add generation

--- processor/src/main/java/com/github/jetlibs/mupped/core/generation/MuppedAnnotationProcessor.java
+++ processor/src/main/java/com/github/jetlibs/mupped/core/generation/MuppedAnnotationProcessor.java
@@ -1,33 +1,71 @@
 package com.github.jetlibs.mupped.core.generation;
 
 import com.github.jetlibs.mupped.annotations.MapTo;
+import com.github.jetlibs.mupped.interfaces.Mapper;
+import com.squareup.javapoet.*;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class MuppedAnnotationProcessor extends AbstractProcessor {
 
     private Messager messager;
+    private Filer filer;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
         messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (Element element : roundEnv.getElementsAnnotatedWith(MapTo.class)) {
             messager.printMessage(Diagnostic.Kind.NOTE, "Found class: " + element.getSimpleName().toString());
-            if (element.getKind() != ElementKind.CLASS) {
-                return true;
+            String fromClassName = element.getSimpleName().toString();
+            String toClassName = "B";
+            String mapperName = String.format("%sto%sMapper", fromClassName, toClassName);
+
+            TypeSpec.Builder navigatorClass = TypeSpec
+                    .classBuilder(mapperName)
+                    .addSuperinterface(ParameterizedTypeName.get(ClassName.get(Mapper.class), TypeName.get(element.asType()), TypeName.get(element.asType())))
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+
+            MethodSpec constructor = MethodSpec
+                    .constructorBuilder()
+                    .addModifiers(Modifier.PUBLIC)
+                    .build();
+
+            MethodSpec mapMethod = MethodSpec
+                    .methodBuilder("map")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addAnnotation(Override.class)
+                    .returns(TypeName.get(element.asType()))
+                    .addParameter(TypeName.get(element.asType()), "input", Modifier.FINAL)
+                    .addCode("return null;")
+                    .build();
+
+            navigatorClass.addMethod(mapMethod);
+            navigatorClass.addMethod(constructor);
+
+            try {
+                JavaFile.builder("com.github.jetlibs.mupped.mappers", navigatorClass.build()).build().writeTo(filer);
+
+                messager.printMessage(Diagnostic.Kind.NOTE, mapperName + " IsGenerated");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
         }
 
         return false;
@@ -40,7 +78,9 @@ public class MuppedAnnotationProcessor extends AbstractProcessor {
         return annotataions;
     }
 
-    @Override public SourceVersion getSupportedSourceVersion() {
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
     }
+
 }
--- sample/src/main/java/com/github/jetlibs/mupped/test/MainTest.java
+++ sample/src/main/java/com/github/jetlibs/mupped/test/MainTest.java
@@ -1,7 +1,10 @@
 package com.github.jetlibs.mupped.test;
 
+import com.github.jetlibs.mupped.mappers.AtoBMapper;
+
 public class MainTest {
     public static void main(String[] args) {
         A a = new A(1, "");
+        new AtoBMapper();
     }
 }
